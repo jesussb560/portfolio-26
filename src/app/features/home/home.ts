e0ee29283d18a50e27f62679b0c46f1a1f36a7f2
jesussb560 <jesus.segurab560@gmail.com>
2025-12-18 13:34:58 -0300

    button component and buttons in home

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Container } from '../../shared/container/container';
+import { Button } from '../../shared/button/button';
 
 @Component({
   selector: 'app-home',
-  imports: [Container],
+  imports: [Container, Button],
   standalone: true,
   templateUrl: './home.html',
 })
---
+++ src/app/shared/button/button.ts
@@ -0,0 +1,44 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  template: `
+    <button
+        type="button"
+        [class]="classes"
+      >
+        <ng-content></ng-content>
+    </button>
+    `,
+  styleUrl: './button.scss',
+})
+export class Button {
+
+  @Input() variant: 'primary' | 'ghost' | 'icon' = 'primary';
+  @Input() size: 'sm' | 'md' | 'lg' = 'md';
+
+  get classes(): string {
+    const base =
+      'inline-flex items-center justify-center gap-2 transition ' +
+      'disabled:opacity-50 disabled:pointer-events-none';
+
+    const variants = {
+      primary: 'rounded-lg bg-dark-top hover:bg-dark-top/90',
+      ghost: 'rounded-md text-white/80 ',
+      icon: 'rounded-full border border-white/20 hover:bg-white/10'
+    };
+
+    const sizes = {
+      sm: 'px-3 py-1.5 text-sm',
+      md: 'px-4 py-2 text-sm',
+      lg: 'px-5 py-2.5 text-base'
+    };
+
+    const iconSize = this.variant === 'icon' ? 'size-11 p-0' : sizes[this.size];
+
+    return `${base} ${variants[this.variant]} ${iconSize}`;
+  }
+
+}
