4e81d3e0614b339aece02e10c48703f61df9feb9
jesussb560 <jesus.segurab560@gmail.com>
2025-12-18 12:10:27 -0300

    initial color and fonts structure

---
+++ src/app/features/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Container } from '../../shared/container/container';
+
+@Component({
+  selector: 'app-home',
+  imports: [Container],
+  standalone: true,
+  templateUrl: './home.html',
+})
+export class Home {
+
+}
