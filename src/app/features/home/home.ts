8c01c538c911e8c92ad392583374f0a6292a9005
jesussb560 <jesus.segurab560@gmail.com>
2025-12-18 22:50:34 -0300

    Card component and its implementation on home component

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { Container } from '../../shared/container/container';
-import { Button } from '../../shared/button/button';
+import { Container } from '../../shared/ui/container/container';
+import { ContactActions } from '../../shared/components/contact-actions/contact-actions';
+import {Card} from '../../shared/ui/card/card';
 
 @Component({
   selector: 'app-home',
-  imports: [Container, Button],
+  imports: [Container, ContactActions, Card],
   standalone: true,
   templateUrl: './home.html',
 })
