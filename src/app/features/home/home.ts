41705a163b01433941d74e50b3a933d78c6340f2
jesussb560 <jesus.segurab560@gmail.com>
2025-12-19 22:20:03 -0300

    Vertical stepper component

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -3,10 +3,11 @@ import { Container } from '../../shared/ui/container/container';
 import { ContactActions } from '../contact-actions/contact-actions';
 import {Projects} from '../projects/projects';
 import {Stack} from '../stack/stack';
+import {Experience} from '../experience/experience';
 
 @Component({
   selector: 'app-home',
-  imports: [Container, ContactActions, Projects, Stack],
+  imports: [Container, ContactActions, Projects, Stack, Experience],
   standalone: true,
   templateUrl: './home.html',
 })
